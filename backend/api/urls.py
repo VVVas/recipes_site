f6e9eefcca60cf04ef22f1684e1d2f71864eb281
VVVas <v@vvvas.ru>
2023-09-18 01:18:33 +0300

    api tag

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -2,13 +2,14 @@
 from django.urls import include, path
 from rest_framework import routers
 
-from .views import IngredientViewSet
+from .views import IngredientViewSet, TagViewSet
 
 app_name = 'api'
 
 router = routers.DefaultRouter()
 
 router.register('ingredients', IngredientViewSet, basename='ingredients')
+router.register('tags', TagViewSet, basename='tags')
 
 urlpatterns = [
     path('', include(router.urls)),
