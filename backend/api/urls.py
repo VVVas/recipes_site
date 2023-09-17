478b1e88b7cffd8aaf3e8d4921db675e18f690b9
VVVas <v@vvvas.ru>
2023-09-18 01:27:35 +0300

    api basename ingredient and tag

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -8,8 +8,8 @@ app_name = 'api'
 
 router = routers.DefaultRouter()
 
-router.register('ingredients', IngredientViewSet, basename='ingredients')
-router.register('tags', TagViewSet, basename='tags')
+router.register('ingredients', IngredientViewSet, basename='ingredient')
+router.register('tags', TagViewSet, basename='tag')
 
 urlpatterns = [
     path('', include(router.urls)),
